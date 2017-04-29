d8b96201300fb46f226dd1b9a176e3a91755beba
Deanna Arora <aroradeanna2@gmail.com>
2017-04-30 02:17:55 +1000

    Finished deliver to

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,3 +1,10 @@
 $(".content1").on("click", function(){
 $(".content1e").toggleClass("expanded");
 });
+
+$(".content3").on("click", function(){
+$(".content3e").toggleClass("expanded");
+});
+$(".content6").on("click", function(){
+$(".content6e").toggleClass("expanded");
+});
