bbfe2e6b3c79a012fdc5ad6d246e9a0374f7e160
Kosta Dunn <kostacoffee@gmail.com>
2017-04-29 15:24:28 +1000

    Started project

---
+++ run.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+app.debug = True
+
+@app.route("/")
+def hello():
+	return render_template('hello.jade')
+
+if __name__ == '__main__':
+	app.run()
